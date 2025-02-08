47bf30152751743b79c7caaeac94b275b4d1933e
fletcherdeal92 <30945271+fletcherdeal92@users.noreply.github.com>
2025-02-08 17:36:58 -0500

    Add round functionality

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,11 @@
 //Rock, Paper, Scissors
 
-// Determine the computers choice
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
 
+
+// Determine the computers choice
 const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3);
 
@@ -24,6 +28,7 @@ const getComputerChoice = () => {
     }
 }
 
+// get the choice of the player
 const getHumanChoice = () => {
     let playerInput = prompt("What's your move? Rock, Paper, or Scissors?")
     if (playerInput.toLowerCase() === 'rock' || playerInput.toLowerCase() === 'paper' || playerInput.toLowerCase() === 'scissors') {
@@ -33,5 +38,49 @@ const getHumanChoice = () => {
     }
 }
 
-console.log(getHumanChoice());
+// Play a round of RPS
+function playRound(humanChoice, computerChoice) {
+
+    let message = `You picked ${humanChoice}, and the computer picked ${computerChoice}.`;
+    round ++;
+
+    if (humanChoice === computerChoice) {
+         message = `You both picked ${humanChoice}, this game is tied. No points`;
+    } 
+
+    if (humanChoice === 'rock' && computerChoice === 'paper') {
+        message += ' The Computer wins this round.';
+        computerScore ++;
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        message += ' Congratulations, you win this round';
+        humanScore ++;
+    }
+    
+    if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        message += ' The Computer wins this round.';
+        computerScore ++;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        message += ' Congratulations, you win this round';
+        humanScore ++;
+    }
+    
+    if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        message += ' The Computer wins this round.';
+        computerScore ++;
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        message += ' Congratulations, you win this round';
+        humanScore ++;
+    }
+    console.log(`Round: ${round}`);
+    console.log(`Computer: ${computerScore}.    Player: ${humanScore}.`);
+    console.log(message);
+    return message;
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
+
 
