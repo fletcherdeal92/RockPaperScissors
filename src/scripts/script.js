bbe65ebfb37da3eca0362242b232c5bd9aa45683
fletcherdeal92 <30945271+fletcherdeal92@users.noreply.github.com>
2025-02-08 16:54:30 -0500

    User can now input their choice with a prompt

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,37 @@
 //Rock, Paper, Scissors
 
-//Get the players choice
-//Get the computers choice
-//determine the winner
-//display the results in a console.log
+// Determine the computers choice
+
+const getComputerChoice = () => {
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    switch (computerChoice) {
+        case 0:
+            return 'rock';
+            break;
+
+        case 1:
+            return 'paper';
+            break;
+        
+        case 2:
+            return 'scissors';
+            break;
+
+        default:
+            return `something weird happened and the computer got ${computerChoice}`;
+            break;
+    }
+}
+
+const getHumanChoice = () => {
+    let playerInput = prompt("What's your move? Rock, Paper, or Scissors?")
+    if (playerInput.toLowerCase() === 'rock' || playerInput.toLowerCase() === 'paper' || playerInput.toLowerCase() === 'scissors') {
+        return playerInput.toLowerCase()
+    } else {
+        return `You input "${playerInput}," which is invalid. Please use Rock, Paper, or Scissors.`
+    }
+}
+
+console.log(getHumanChoice());
+
