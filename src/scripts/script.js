6baa06d8d25a55560d1c2f3b415b7c07c4e2f1b4
Fletcher Deal <fad92@Fletchers-Mac-mini.local>
2025-03-13 22:44:51 -0400

    updating RPS

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,11 +5,17 @@ let humanScore = 0;
 let computerScore = 0;
 let playerRounds = 5;
 let round = 0;
-let emoji = "";
+let roundName = 'one';
+let emoji = '';
+let humanInput = '';
+let computerInput = '';
 
-const rockrEmoji = "✊";
-const paperEmoji = "🖐️";
-const sciEmoji = "✌️";
+let state = '';
+let gameOver = false;
+
+const rockrEmoji = '✊';
+const paperEmoji = '🖐️';
+const sciEmoji = '✌️';
 
 
 // Determine the computers choice
@@ -53,85 +59,166 @@ function playRound(humanChoice, computerChoice) {
     let message = `You picked ${humanChoice}, and the computer picked ${computerChoice}.`;
     round ++;
 
+    humanInput = humanChoice;
+    computerInput = computerChoice;
+
     if (humanChoice === computerChoice) {
          message = `You both picked ${humanChoice}, this game is tied. No points`;
+         state = 'tie';
     } 
 
     if (humanChoice === 'rock' && computerChoice === 'paper') {
         message += ' The Computer wins this round.';
+        state = 'lose'
         computerScore ++;
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
         message += ' Congratulations, you win this round';
+        state = 'win';
         humanScore ++;
     }
     
     if (humanChoice === 'paper' && computerChoice === 'scissors') {
         message += ' The Computer wins this round.';
+        state = 'lose';
         computerScore ++;
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
         message += ' Congratulations, you win this round';
+        state = 'win';
         humanScore ++;
     }
     
     if (humanChoice === 'scissors' && computerChoice === 'rock') {
         message += ' The Computer wins this round.';
+        state = 'lose';
         computerScore ++;
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         message += ' Congratulations, you win this round';
+        state = 'win';
         humanScore ++;
     }
     console.log(`Round: ${round}`);
     console.log(`Computer: ${computerScore}.    Player: ${humanScore}.`);
-    console.log(message);
+    console.log(message + ` state is ${state}`);
     return message;
     
 }
 
+// TranslateEmoji
+function translateEmoji(i) {
+    
+    if (i === 'rock') {
+        emoji = rockrEmoji;
+    } else if (i === 'paper') {
+        emoji = paperEmoji;
+    } else if (i === 'scissors') {
+        emoji = sciEmoji;
+    } else if (i === '') {
+        emoji = "?";
+    } else {
+        emoji = "🤷";
+    }
+    return emoji;
+}
+
+function translateNumber(x)  {
+    switch (x) {
+        case 0:
+            roundName = "one";
+            break;
+        case 1:
+            roundName = "two";
+            break;
+        case 2:
+            roundName = "three";
+            break;
+        case 3:
+            roundName = "four";
+            break;
+        case 4:
+            roundName = "five";
+            break;
+        case 5:
+            roundName = "six";
+            break;
+        default:
+            roundName = "  ${x}  ";
+            break;
+    }
+    return roundName;
+}
+
+ function winSatateEffects() {
+    console.log(`state is ${state}`);
+    let winner = 'winRound'
+    let loser = 'loseRound'
+    if (state === 'win') {
+        document.querySelector('#roundWindow').classList.add(`${winner}`);
+        document.querySelector('#roundWindow').classList.remove(`${loser}`);
+        console.log(`state is ${state}`);
+    } else if (state === 'lose') {
+        document.querySelector('#roundWindow').classList.add(`${loser}`);
+        document.querySelector('#roundWindow').classList.remove(`${winner}`);
+        console.log(`state is ${state}`);
+    } else {
+        document.querySelector('#roundWindow').classList.remove(`${winner}`);
+        document.querySelector('#roundWindow').classList.remove(`${loser}`);
+        console.log(`state is ${state}`);
+    }
+}
+
+function updateDOM() {
+    const playerScore = document.querySelector('#playerScore');
+    const pcScore = document.querySelector('#computerScore');
+    const roundDOM = document.querySelector('#round');
+    const humanEmojiDOM = document.querySelector('.playerChoice');
+    const computerEmojiDOM = document.querySelector('.computerChoice');
+    
+    playerScore.textContent = `${humanScore}`;
+    pcScore.textContent = `${computerScore}`
+    roundDOM.textContent = `${translateNumber(round)}`;
+    computerEmojiDOM.textContent = `${translateEmoji(computerInput)}`;
+    humanEmojiDOM.textContent = `${translateEmoji(humanInput)}`;
+}
+
+/* 
+
+function checkGameState() {
+    if gameOver {
+
+    }
+} 
+    
+*/
+
 function playGame(e) {
-    console.log(e.id)
+    console.log(e.id);
     if (round >= playerRounds - 1) {
         console.log(`Game Over! Final Score: Computer: ${computerScore}. Player: ${humanScore}.`);
     } else {
         const humanSelection = e.id;
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
+        checkGameState();
+        updateDOM();
+        winSatateEffects();
+        console.log(`${computerScore}`)
     }
 
 }
 
-//playGame();
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    playerRounds = 5;
+    round = 0;
+    roundName = 'one';
+    emoji = '';
+    humanInput = '';
+    computerInput = '';
+    state = '';
+    gameOver = false;
+    updateDOM();
+    winSatateEffects();
+}
 
-const container = document.querySelector('.container2');
-const p = document.createElement('p');
-const h3 = document.createElement('h3');
-const h1 = document.createElement('h1');
-
-const content = document.createElement('div');
-content.classList.add('testContent');
-
-h1.textContent = `I'm in a DIV`;
-p.textContent = `ME TOO!`;
-content.appendChild(h1);
-content.appendChild(p);
-
-
-let paraContent = document.createElement('p');
-paraContent.classList.add('redText');
-paraContent.textContent = `Hey I'm red!`;
-
-let headerContent = h3;
-headerContent.classList.add('blueText');
-headerContent.textContent = `I'm blue!`;
-
-container.appendChild(paraContent);
-container.appendChild(headerContent);
-container.appendChild(content);
-
-const btn = document.querySelector('#btn');
-const btn2 = document.querySelector('#btn2');
-
-btn.onclick = () => alert('Hello World!');
-
-btn2.addEventListener('click', () => {
-    alert('Hello World!');
-});
+updateDOM();
